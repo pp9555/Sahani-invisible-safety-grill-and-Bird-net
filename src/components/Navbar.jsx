b7b3650dfcc60002affa37045fb6ffc748bdbb6f
pp9555 <160492858+pp9555@users.noreply.github.com>
2025-11-06 20:03:09 +0530

    Initial commit - SkySafe Grills demo website final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
